b0f0fdfa59e33c44b9d2949c3258fa6dd645ae7b
preethi2001-collab <72606635+preethi2001-collab@users.noreply.github.com>
2024-01-12 23:15:23 +0530

    Assignment Completed-DOM Manipulation

---
+++ DOM-Manipulation/script.js
@@ -0,0 +1,12 @@
+console.log("hello!!!");
+let box = document.getElementById("box");
+let heading = document.getElementById("head1");
+box.style.height = "200px";
+box.style.width = "300px"
+box.style.backgroundColor = "yellow";
+box.style.margin = "20px";
+box.style.padding = "10px";
+heading.style.fontSize = "18px";
+heading.style.fontWeight = "bold";
+heading.style.margin="0px";
+heading.style.color = "blue";
